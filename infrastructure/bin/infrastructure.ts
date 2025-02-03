aa37397fd0952633f4b43c87696784105b4cc1ee
Davide De Pieri <davide.depieri@pixartprinting.com>
2025-02-03 15:34:26 +0100

    first commit

---
+++ infrastructure/bin/infrastructure.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+import 'source-map-support/register'
+import { App } from 'aws-cdk-lib'
+import { Pipeline } from '../lib/pipeline'
+import { Application, Dns } from '../lib/application'
+import { ConfigHelper, Tier } from '@pixartprinting-devops/cdk-constructs'
+// import { ConfigHelper, Tier } from '../constructs/configHelper'
+
+console.log('Hello')
+const app = new App()
+const tier = app.node.tryGetContext('tier')
+const config = new ConfigHelper('config.json')
+
+switch (tier) {
+  case Tier.PIPELINE:
+    new Pipeline(app, config)
+    break
+  case Tier.STAGING:
+    
+    break
+  case Tier.PRODUCTION:
+    
+    break
+  default:
+    console.log('Please define the tier context: production | staging | pipeline. es: --context tier=pipeline')
+    break
+}
